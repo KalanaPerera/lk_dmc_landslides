a633ff1e63abca086f827886eaf0f78682455711
nuuuwan <nuuuwan@gmail.com>
2025-12-02 17:36:56 +0530

    Add __init__.py files for lk_dmc and lk_dmc.core modules to initialize package structure

---
+++ src/lk_dmc/__init__.py
@@ -0,0 +1,4 @@
+# lk_dmc (auto generate by build_inits.py)
+# flake8: noqa: F408
+
+from lk_dmc.core import LandslideWarning, ThreatLevel
---
+++ src/lk_dmc/core/__init__.py
@@ -0,0 +1,5 @@
+# lk_dmc.core (auto generate by build_inits.py)
+# flake8: noqa: F408
+
+from lk_dmc.core.LandslideWarning import LandslideWarning
+from lk_dmc.core.ThreatLevel import ThreatLevel
